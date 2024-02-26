c3d4976c9f7f39fdc636dab6c33839a44699cd73
Lolanimus <94278422+Lolanimus@users.noreply.github.com>
2024-02-26 14:27:28 -0500

    Added zip validation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const error = document.createElement("span");
 const email = document.getElementById("mail");
 const country = document.getElementById("country");
+const zip = document.getElementById("zip");
 
 email.addEventListener("input", e => {
     if (email.validity.typeMismatch) {
@@ -16,6 +17,16 @@ country.addEventListener("input", e => {
     if (country.validity.tooShort) {
         error.innerHTML = "Too short name for a country";
         country.insertAdjacentElement("afterend", error);
+        e.preventDefault();
+    } else {
+        error.remove();
+    }
+})
+
+zip.addEventListener("input", e => {
+    if (zip.validity.tooShort || zip.validity.tooLong) {
+        error.innerHTML = "Sorry, we support only American ZIP";
+        zip.insertAdjacentElement("afterend", error);
         e.preventDefault();
     } else {
         error.remove();
