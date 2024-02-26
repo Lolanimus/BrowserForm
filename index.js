d854ed0932c9a4403be95bd8b965df9016ede489
Lolanimus <94278422+Lolanimus@users.noreply.github.com>
2024-02-26 14:26:20 -0500

    Added country validation

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const error = document.createElement("span");
 const email = document.getElementById("mail");
 const country = document.getElementById("country");
-const zip = document.getElementById("zip");
-const pswrd = document.getElementById('psword');
-const pswrdReg = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
 
 email.addEventListener("input", e => {
     if (email.validity.typeMismatch) {
@@ -24,30 +21,3 @@ country.addEventListener("input", e => {
         error.remove();
     }
 })
-
-zip.addEventListener("input", e => {
-    if (zip.validity.tooShort || zip.validity.tooLong) {
-        error.innerHTML = "Sorry, we support only American ZIP";
-        zip.insertAdjacentElement("afterend", error);
-        e.preventDefault();
-    } else {
-        error.remove();
-    }
-})
-
-pswrd.addEventListener("input", e => {
-    console.log(pswrd.validity);
-    if (pswrd.validity.tooShort) {
-        error.innerHTML = "Password is too short";
-        pswrd.insertAdjacentElement("afterend", error);
-        e.preventDefault();
-    } else if (pswrdReg.test(pswrd)) {
-        error.innerHTML = "Password should contain";
-        pswrd.insertAdjacentElement("afterend", error);
-        e.preventDefault();
-    } else {
-        error.remove();
-    }
-
-})
-
