3cd9c6df8ea0129f202e9337a61d64de90c0ed48
Lolanimus <94278422+Lolanimus@users.noreply.github.com>
2024-02-26 14:57:40 -0500

    Changed the way how the error works(created span for each input element and added hidden class to it)

--- index.js
+++ index.js
@@ -1,62 +1,67 @@
-const error = document.createElement("span");
 const email = document.getElementById("mail");
 const country = document.getElementById("country");
 const zip = document.getElementById("zip");
 const pswrd = document.getElementById('pswrd');
 const pswrdConf = document.getElementById("pswrdConf");
+const submitBtn = document.getElementById("submitBtm");
+const form = document.querySelector('form');
 const pswrdReg = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
 
 email.addEventListener("input", e => {
+    const error = document.querySelector("#mail + span");
     if (email.validity.typeMismatch) {
         error.innerHTML = "Input correct email";
-        email.insertAdjacentElement("afterend", error);
+        error.classList.remove("hidden");
         e.preventDefault();
     } else {
-        error.remove();
+        error.classList.add("hidden");
     }
 })
 
 country.addEventListener("input", e => {
+    const error = document.querySelector("#country + span");
     if (country.validity.tooShort) {
         error.innerHTML = "Too short name for a country";
-        country.insertAdjacentElement("afterend", error);
+        error.classList.remove("hidden");
         e.preventDefault();
     } else {
-        error.remove();
+        error.classList.add("hidden");
     }
 })
 
 zip.addEventListener("input", e => {
+    const error = document.querySelector("#zip + span");
     if (zip.validity.tooShort || zip.validity.tooLong) {
         error.innerHTML = "Sorry, we support only American ZIP";
-        zip.insertAdjacentElement("afterend", error);
+        error.classList.remove("hidden");
         e.preventDefault();
     } else {
-        error.remove();
+        error.classList.add("hidden");
     }
 })
 
 pswrd.addEventListener("input", e => {
-    console.log(pswrd.validity);
+    const error = document.querySelector("#pswrd + span");
     if (pswrd.validity.tooShort) {
         error.innerHTML = "Password is too short";
-        pswrd.insertAdjacentElement("afterend", error);
+        error.classList.remove("hidden");
         e.preventDefault();
     } else if (!pswrdReg.test(pswrd.value)) {
         error.innerHTML = "Password should contain at least 1 number, sign and letter";
-        pswrd.insertAdjacentElement("afterend", error);
+        error.classList.remove("hidden");
         e.preventDefault();
     } else {
-        error.remove();
+        error.classList.add("hidden");
     }
 })
 
 pswrdConf.addEventListener("input", e => {
+    const error = document.querySelector("#pswrdConf + span");
     if (pswrdConf.value != pswrd.value) {
         error.innerHTML = "Passwords are not equal";
-        pswrdConf.insertAdjacentElement("afterend", error);
+        error.classList.remove("hidden");
         e.preventDefault();   
     } else {
-        error.remove();
+        error.classList.add("hidden");
     }
 })
